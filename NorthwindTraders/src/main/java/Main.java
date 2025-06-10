9e2134d24b7c6c32af483d051fc6d2c01d1e9028
MoFunCode <mohamed.sul02@gmail.com>
2025-06-10 09:58:51 -0500

    Modified exercise 1 to use PreparedStatement

--- NorthwindTraders/src/main/java/Main.java
+++ NorthwindTraders/src/main/java/Main.java
@@ -2,25 +2,36 @@ import java.sql.*;
 
 public class Main {
 
-    public static void main(String[] args) throws ClassNotFoundException{
+    public static void main(String[] args) throws ClassNotFoundException {
 
         Class.forName("com.mysql.cj.jdbc.Driver");
         String userName = "root";
         String password = "yearup24";
         String url = "jdbc:mysql://localhost:3306/sakila";
-        String queryTest = "SELECT * FROM northwind.Products";
+
+        String queryTest = "SELECT ProductID, ProductName, UnitPrice, UnitsINStock FROM northwind.Products";
 
         try {
 
             Connection connectionToNordwind = DriverManager.getConnection(url, userName, password);
-            Statement statement = connectionToNordwind.createStatement();
-            ResultSet resultSet = statement.executeQuery(queryTest);
+            PreparedStatement preparedStatement = connectionToNordwind.prepareStatement(queryTest);
+            ResultSet resultSet = preparedStatement.executeQuery();
 
+            //Statement statement = connectionToNordwind.createStatement();
+            //ResultSet resultSet = statement.executeQuery(queryTest);
 
             while (resultSet.next()) {
+                int productID = resultSet.getInt("ProductID");
+                String productName = resultSet.getString("ProductName");
+                double unitPrice = resultSet.getDouble("UnitPrice");
+                int unitsInStock = resultSet.getInt("UnitsInStock");
 
-                String firstName = resultSet.getString("ProductName");
-                System.out.println(firstName);
+                System.out.println("Product Id: " + productID);
+                System.out.println("Name: " + productName);
+                System.out.println("Price: " + unitPrice);
+                System.out.println("Stock: " + unitsInStock);
+                // String firstName = resultSet.getString("ProductName");
+                // System.out.println(firstName);
             }
 
         } catch (SQLException e) {
