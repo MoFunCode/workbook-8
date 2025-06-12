93f840a3cd44618e35bdf6fe63cc0de9b1876b30
MoFunCode <mohamed.sul02@gmail.com>
2025-06-12 10:29:43 -0500

    add displayMoviesByActorFirstAndLastName()

--- SakilaMovies/src/main/java/Main.java
+++ SakilaMovies/src/main/java/Main.java
@@ -1,4 +1,5 @@
 import org.apache.commons.dbcp2.BasicDataSource;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -9,16 +10,33 @@ public class Main {
 
     private static final String userName = "root";
     private static final String password = "y=hehhe";
-    private static final String url = "jdbc:mysql://localhost:3306/Sakila";
+    private static final String url = "jdbc:mysql://localhost:3306/sakila";
+    private static final Scanner sc = new Scanner(System.in);
     private static BasicDataSource dataSource;
-    private static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
+
         setUpDataSourceCon();
 
-        System.out.println("Please enter a last name of an actor you like:");
-        String lastName = sc.nextLine().trim();
-        displayActorsByLastName(lastName);
+        System.out.println("What would you like to do?");
+        System.out.println("1. Search actors by last name");
+        System.out.println("2. Search movies by actor name");
+        int choice = getIntInput();
+
+        switch (choice) {
+            case 1:
+                System.out.println("Please enter a last name of an actor you like:");
+                String lastName = sc.nextLine().trim();
+                displayActorsByLastName(lastName);
+                break;
+            case 2:
+                System.out.println("Enter first name of actor:");
+                String actorFirstName = sc.nextLine().trim();
+                System.out.println("Enter last name of actor:");
+                String actorLastName = sc.nextLine().trim();
+                displayMoviesByActorFirstAndLastName(actorFirstName, actorLastName);
+                break;
+        }
     }
 
     public static void setUpDataSourceCon() {
@@ -57,7 +75,48 @@ public class Main {
         }
     }
 
-    public int getIntInput() {
+    public static void displayMoviesByActorFirstAndLastName(String actorFirstName, String actorLastName) {
+        String sqlQuery = """
+                SELECT f.film_id, f.title, f.description, f.release_year
+                FROM film f
+                INNER JOIN film_actor fa ON f.film_id = fa.film_id
+                INNER JOIN actor a ON fa.actor_id = a.actor_id
+                WHERE a.first_name = ? AND a.last_name = ?
+                ORDER BY f.title
+                """;
+
+        try (Connection con = dataSource.getConnection();
+             PreparedStatement statement = con.prepareStatement(sqlQuery)) {
+
+            statement.setString(1, actorFirstName);
+            statement.setString(2, actorLastName);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+
+                do {
+                    int filmId = resultSet.getInt("film_id");
+                    String title = resultSet.getString("title");
+                    String description = resultSet.getString("description");
+                    int releaseYear = resultSet.getInt("release_year");
+
+                    System.out.println("Film Id: " + filmId);
+                    System.out.println("Title: " + title);
+                    System.out.println("Description: " + description);
+                    System.out.println("Release Year: " + releaseYear);
+
+                } while (resultSet.next());
+
+            } else {
+                System.out.println("No movies found for actor: " + actorFirstName + " " + actorLastName);
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Database error: " + e.getMessage());
+        }
+    }
+
+    public static int getIntInput() {
         try {
             String input = sc.nextLine().trim();
             return Integer.parseInt(input);
@@ -66,4 +125,5 @@ public class Main {
             return -1;
         }
     }
+
 }
